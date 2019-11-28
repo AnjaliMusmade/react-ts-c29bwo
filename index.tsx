d9470f9c5693dc1d03951c272ebca2197ee4eeaa
AnjaliMusmade <anjalimusmade29@gmail.com>
2019-11-28 11:33:03 +0530

    1 file modified

--- index.tsx
+++ index.tsx
@@ -7,7 +7,7 @@ var oldObj={
   LastNamne:"Musmade",
   Salary:100
 };
-
+//cloning using spread
 var newObj={...oldObj,Salary:2000};
 
 console.log(oldObj);
@@ -18,6 +18,20 @@ var arr2=[...arr,5,6,7];
 
 console.log(arr);
 console.log(arr2);
+
+var objAssigned=Object.assign({},oldObj,{Salary:4000,Project:"ABC"});
+console.log(objAssigned);
+//obj destructuring
+//using a form of spread to gety remaning all too in variables if large  num of props.
+
+let {FirstNAme,...rest}=oldObj;
+console.log(rest);
+
+//rename the property
+let {FirstNAme,LastNamne:lname,...remaining}=objAssigned;
+console.log(remaining);
+console.log(remaining.Salary);
+console.log(lname);
 
 ReactDOM.render( <div>Hello</div>,document.getElementById('root'));
 
